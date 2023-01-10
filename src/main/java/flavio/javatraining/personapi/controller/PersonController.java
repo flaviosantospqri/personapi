4cfff81b742807d12c3e72c3ecd59fa9ab9df4c4
Flavio Santos <flavio.pasantos@outlook.com>
2023-01-10 15:14:22 -0300

    created rout for create person

--- src/main/java/flavio/javatraining/personapi/controller/PersonController.java
+++ src/main/java/flavio/javatraining/personapi/controller/PersonController.java
@@ -1,5 +1,6 @@
 package flavio.javatraining.personapi.controller;
 
+import flavio.javatraining.personapi.dto.MessageResponseDTO;
 import flavio.javatraining.personapi.model.Person;
 import flavio.javatraining.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +13,17 @@ public class PersonController {
 
     private PersonRepository personRepository;
 
+
+    //injeção de dependencias.
     @Autowired
     public PersonController(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
     @PostMapping
-    public String createPerson(@RequestBody Person person){
+    public MessageResponseDTO createPerson(@RequestBody Person person){
        Person personSave = personRepository.save(person);
-        return "Teste API";
+        return MessageResponseDTO.builder().message("Created person with ID" + personSave.getId()).build();
     }
 
 }
